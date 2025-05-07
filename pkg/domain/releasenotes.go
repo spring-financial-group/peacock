829510ca5d23306681b743e17c9b10a10e51ec31
Skisocks <jaa.skeoch@gmail.com>
2025-05-07 11:46:57 +0100

    feat: expose function for merging new notes into existing markdown

--- pkg/domain/releasenotes.go
+++ pkg/domain/releasenotes.go
@@ -10,7 +10,7 @@ type ReleaseNotesUseCase interface {
 	// PopulateTeamsInReleaseNotes populates the teams in the release notes with the corresponding teams in feathers
 	PopulateTeamsInReleaseNotes(releaseNotes []models.ReleaseNote, teamsInFeathers models.Teams) error
 	// ParseReleaseNoteFromMarkdown parses release notes from a markdown string
-	ParseReleaseNoteFromMarkdown(markdown string) ([]models.ReleaseNote, error)
+	ParseReleaseNoteFromMarkdown(markdown string, sanitise bool) (preamble string, notes []models.ReleaseNote, err error)
 	// GetMarkdownFromReleaseNotes generates a markdown string from a slice of release notes
 	GetMarkdownFromReleaseNotes(notes []models.ReleaseNote) string
 	// GenerateHash generates a SHA256 hash of the json of a slice of release notes
@@ -19,7 +19,7 @@ type ReleaseNotesUseCase interface {
 	GenerateBreakdown(notes []models.ReleaseNote, hash string, totalTeams int) (string, error)
 	// SendReleaseNotes sends release notes to their respective teams
 	SendReleaseNotes(subject string, notes []models.ReleaseNote) error
-	// AppendReleaseNotesToExisting appends new release notes to existing ones if the teams are the same
-	// If the teams are different, then the note isn't appended
-	AppendReleaseNotesToExisting(existing, new []models.ReleaseNote) []models.ReleaseNote
+	// AppendReleaseNotesToExistingMarkdown appends release notes to an existing markdown string merging notes by team if possible.
+	// If a note is not mergable, it will be appended as a new note. Order of the existing notes is preserved.
+	AppendReleaseNotesToExistingMarkdown(existingMarkdown string, releaseNotesToAppend []models.ReleaseNote) (string, error)
 }
